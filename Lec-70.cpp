8970875060bc0374e467c7f98fb4b8f932f91377
Pratham Mevada <prathammevada34@gmail.com>
2025-10-12 20:23:13 +0530

    Initial-commit

---
+++ Lec-70.cpp
@@ -0,0 +1,20 @@
+// OOPS lecture-1 Learning basics of oops
+#include <iostream>
+using namespace std;
+
+class Student
+{
+  string name;
+  int age,roll_number;
+  char grade;
+};
+
+
+int main()
+{
+  string name;
+  int age,roll_number;
+  char grade;
+
+  cin>>name>>age>>roll_number>>grade;
+}

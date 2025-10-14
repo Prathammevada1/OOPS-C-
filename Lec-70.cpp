0b725ec2653090676f19774fd0bb70d5294e16c9
Pratham Mevada <prathammevada34@gmail.com>
2025-10-14 12:42:27 +0530

    Padding concept

--- Lec-70.cpp
+++ Lec-70.cpp
@@ -2,27 +2,33 @@
 #include <iostream>
 using namespace std;
 
+// if char c int b char d double e 
+// c p p p b b b b d p p p p p p p e e e e e e e e 
+// e takes memory from multiple of 8 divible by the byte it takes memory like int takes 
+// memory from multiple of 4;
+// 24 bytes
 class Student
 {
-
-
+  // Greedy Allignment
+  // First write the datatype that takes more space and in descending order
+  
+  char b;
+  int a;
+  char c;
+  double e;
 };
 
 
 int main()
 {
   
-  // Right now no memory is allocated for class it is only allocated when we make object of that class
+  // Concept of padding
+  // compiler does this when writtten char a int b;
+  // it gives 1 byte to char and padding of 3 byte(for 32 bit os ) it reads in segment
+  // of 4 bytes.Now if we write char a int b char c it takes 12 bytes char 1 byte with 3 padding
+  // int 4 byte and char 1 byte with three padding but compiler takes nearest divisible 
+  // number by 4 to 9 which is 12;
+  // Instead if we write int a char b char c int takes 4 bytes and char 2 byte with padding of 2 
   Student obj1;
   cout<<sizeof(obj1)<<endl;
-
-  // Now 4 bytes of memory is allocated as we made an object;
-  // If nothing is in the class it will still take 1 byte of memory
-// Explanation - We know int takes four bytes lets says int a is stored at 1000 address and till 1003
-// for class if we make Student a;Student b; if no memory is allocated to them then how to 
-// differentiate the objects like obj1 have 0 memory space then it wont exist in address
-// & obj1 would be undefined and also goes for obj2 ;
-// Its like making obj1 but never giving it space to form it wont exist
-  Student obj2;
-
 }

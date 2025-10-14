30038038962370ea0739ca9644092c9a7e045302
Pratham Mevada <prathammevada34@gmail.com>
2025-10-14 12:23:23 +0530

    Space

--- Lec-70.cpp
+++ Lec-70.cpp
@@ -15,9 +15,14 @@ int main()
   // Right now no memory is allocated for class it is only allocated when we make object of that class
   Student obj1;
   cout<<sizeof(obj1)<<endl;
+
   // Now 4 bytes of memory is allocated as we made an object;
   // If nothing is in the class it will still take 1 byte of memory
-
-  
+// Explanation - We know int takes four bytes lets says int a is stored at 1000 address and till 1003
+// for class if we make Student a;Student b; if no memory is allocated to them then how to 
+// differentiate the objects like obj1 have 0 memory space then it wont exist in address
+// & obj1 would be undefined and also goes for obj2 ;
+// Its like making obj1 but never giving it space to form it wont exist
+  Student obj2;
 
 }

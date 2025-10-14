4f502f744c1968e5bfd524338f3f104837dbc273
Pratham Mevada <prathammevada34@gmail.com>
2025-10-14 12:48:20 +0530

    Staticvs Dynamic

--- Lec-70.cpp
+++ Lec-70.cpp
@@ -2,33 +2,21 @@
 #include <iostream>
 using namespace std;
 
-// if char c int b char d double e 
-// c p p p b b b b d p p p p p p p e e e e e e e e 
-// e takes memory from multiple of 8 divible by the byte it takes memory like int takes 
-// memory from multiple of 4;
-// 24 bytes
 class Student
 {
-  // Greedy Allignment
-  // First write the datatype that takes more space and in descending order
-  
-  char b;
-  int a;
-  char c;
-  double e;
+  public:
+  string name;
 };
 
 
 int main()
 {
-  
-  // Concept of padding
-  // compiler does this when writtten char a int b;
-  // it gives 1 byte to char and padding of 3 byte(for 32 bit os ) it reads in segment
-  // of 4 bytes.Now if we write char a int b char c it takes 12 bytes char 1 byte with 3 padding
-  // int 4 byte and char 1 byte with three padding but compiler takes nearest divisible 
-  // number by 4 to 9 which is 12;
-  // Instead if we write int a char b char c int takes 4 bytes and char 2 byte with padding of 2 
-  Student obj1;
-  cout<<sizeof(obj1)<<endl;
+  // Static vs dynamic memory allocation
+  // int * p =new int p with address pointing to heap
+  // p is in stack and pointer to an address in heap
+  // Now creating objects dynamically
+  Student *s = new Student;
+  (*s).name = "Pratham";
+  // s->name="Rohit"; same as line 19;
+  cout<<s->name<<endl;
 }

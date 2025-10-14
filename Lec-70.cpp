f514113187d74f78d328640d7b60a3ae0acd5d2f
Pratham Mevada <prathammevada34@gmail.com>
2025-10-14 12:09:16 +0530

    Empty class space

--- Lec-70.cpp
+++ Lec-70.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Student
 {
 
-  int b;
+
 };
 
 
@@ -13,7 +13,11 @@ int main()
 {
   
   // Right now no memory is allocated for class it is only allocated when we make object of that class
-  
+  Student obj1;
+  cout<<sizeof(obj1)<<endl;
+  // Now 4 bytes of memory is allocated as we made an object;
+  // If nothing is in the class it will still take 1 byte of memory
+
   
 
 }

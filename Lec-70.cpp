78a69845f5ab18c925b9e36b369cfa7d397e34a7
Pratham Mevada <prathammevada34@gmail.com>
2025-10-14 12:05:40 +0530

    Memory in class

--- Lec-70.cpp
+++ Lec-70.cpp
@@ -4,32 +4,16 @@ using namespace std;
 
 class Student
 {
-  public:
-  string name;
-  int age,roll_number;
-  char grade;
+
+  int b;
 };
 
 
 int main()
 {
-  // Now it is accessible as public is written
-
-  Student s1;
-  s1.name = "Pratham";
-  s1.age = 20;
-  s1.grade = 'A';
-  s1.roll_number = 104;
-
-
-  cout<<s1.age<<" "<<s1.grade<<" "<<s1.name<<" "<<s1.roll_number;
-
-  Student s2;
-  s2.name = "Mann";
-  s2.age = 20;
-  s2.grade = 'A';
-  s2.roll_number = 100;
-
-  cout<<s2.age<<" "<<s2.grade<<" "<<s2.name<<" "<<s2.roll_number;
+  
+  // Right now no memory is allocated for class it is only allocated when we make object of that class
+  
+  
 
 }

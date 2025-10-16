7e49fe68d87eacd1fc05622b6b6df0d40a7e7f00
Pratham Mevada <prathammevada34@gmail.com>
2025-10-16 12:21:45 +0530

    inline const

--- lec-71.cpp
+++ lec-71.cpp
@@ -10,27 +10,35 @@ class Customer
   int accnum;
   int balance;
 // default constructor  
-  Customer()
-  {
-    cout<<"Hi";
-  }
+  // Customer()
+  // {
+  //   cout<<"Hi";
+  // }
   // Parameterised const
-  Customer(string name ,int b,int c)
-  {
-    this->name=name;
-    this->accnum=b;
-    this->balance=c;
-    // if written likw name=name it takes localised name so this->name=name takes left\
-    // one as the current objects attribute and name localised one
+  // Customer(string name ,int b,int c)
+  // {
+  //   this->name=name;
+  //   this->accnum=b;
+  //   this->balance=c;
+  //   // if written likw name=name it takes localised name so this->name=name takes left
+  //   // one as the current objects attribute and name localised one
 
-    
-  }
 
+  // }
+  // If constructor is not made than only it by default makes one else it doesnt
+  // meaning if we created a constructor() compiler doesnt make a default one 
+  // constructor overloading - diff parameters
+
+
+  // INline constructor
+  inline Customer(string a,int b,int c):name(a),accnum(b),balance(c){}
 };
 
 int main()
 {
   Customer c1("Pratham",123,1000);
+  cout<<c1.balance;
 }
 // this is a pointer pointing to the current object c1 
+
 

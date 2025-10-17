846785fd44e8e386b3d1f552f8c972fba4e5cb24
Pratham Mevada <prathammevada34@gmail.com>
2025-10-17 13:59:51 +0530

    static function

--- lec-72.cpp
+++ lec-72.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 using namespace std;
 
+
+// static member function
 class Customer
 {
   string name;
   int balance;
-  //belongs to class not objects all others are part of objects
-  // automatically created when class is created all others are created when objects are made;
+  static int total_balance;
+
 
   public:
-  static int total_balance;
+
   Customer(string name,int balance)
   {
     this->name=name;
     this->balance=balance;
     total_balance+=balance;
+  }
+  static void bal()
+  {
+    cout<<total_balance;
   }
 
   void display()
@@ -22,14 +28,21 @@ class Customer
     cout<<name<<" "<<balance<<" "<<total_balance<<endl;
   }
 };
-
+int Customer::total_balance=0;
 // for public int Customer can be accessed anywhere
 
 int main()
 {
-  int Customer::total_balance=0;
   Customer c1("A",10000);
-  c1.display();
   Customer c2("B",20000);
-  c2.display();
+
+  Customer::bal();
+
 }
+
+// basically the static method can only access the static variables 
+// when static int variable is made the memory is not allocated yet
+// it is allocated when we initialize it 31 line and also literals are const across the program
+//also called compile time varible so to initalize static inside class use
+//static const int var = ... it just do (Customer::var = value)every time whenever it sees
+// customer::var it replace it with value;

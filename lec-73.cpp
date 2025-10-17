88022a41242ee488637826b6832c4711c54acf85
Pratham Mevada <prathammevada34@gmail.com>
2025-10-17 15:55:14 +0530

    Access Modifiers

--- lec-73.cpp
+++ lec-73.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 using namespace std;
 
+class Human
+{
+  public:
+  string name;
+  int age;
+};
+
+class Student:protected Human//Inheritance
+{
+  int roll;
+
+  // copied like 
+  // public + private = private
+  // protected + public =protected;
+  // public +anything =anything;
+  // private string name;
+  // private int age;
+
+
+};
+
 int main()
 {
-  
+
+  Student s;
+  s.age;
+
 }
+// Access modifiers
+// private>protected>public

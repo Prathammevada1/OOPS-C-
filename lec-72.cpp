e4aa667d19fb6ce5831ec16ab88f40e5084b700d
Pratham Mevada <prathammevada34@gmail.com>
2025-10-17 14:25:39 +0530

    abstraction

--- lec-72.cpp
+++ lec-72.cpp
@@ -7,29 +7,25 @@ class Customer
   int balance;
 
   public:
+
   Customer(string a,int b)
   {
     name=a;
     balance=b;
 
   }
-
   void deposit(int amount)
   {
-    if(amount>0)
-    balance+=amount;
-    else
-    cout<<"INvalid";
+
   }
 };
 
+
 int main()
 {
-  Customer a1("A",1000);
-  // if we do public to the variables then a1.balance is accessible everywhere 
-  // here as well a1.balance=-2;
-  a1.deposit(-500);
+  // basically showing output and not the whole implementation 
+
+  // like making a function named deposit to deposit money but not showing the function its in a class 
 }
 
-// encapsulation-wrapping variable and function in class and controlling its access
-// data-hiding-not for hacker for user to give variable correct values
+// Abstraction - displaying essential things and hiding the details

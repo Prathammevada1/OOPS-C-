b5a56a4b6d91b70ab2c5c041ea97dfc78e6b52fb
Pratham Mevada <prathammevada34@gmail.com>
2025-10-18 15:17:54 +0530

    commit-4 virtual keyword

--- lec-75.cpp
+++ lec-75.cpp
@@ -18,16 +18,36 @@ class Dog:public Animal
     cout<<"Bark\n";
   }
 
-  void roti()
-  {
-    cout<<"Hello\n";
-  }
 };
 
 int main()
 {
   Animal *p;
-  p=new Dog();
-  p->roti();   
+  p = new Dog();
+
+  
+
+
+  // Here new Dog () returns an address to pointer p for the dog object
+  // dog object contains animals attributes vpointer dog attributes space
+  // when virtual keyword is not used the pointer points to animal part of
+  // dog objects to run method but when virtual is written it goes for runtime to decide
+  // what to run so 
+  // p is declared as a pointer to Animal.
+
+//  new Dog() allocates a Dog object on the heap and returns a Dog*.
+
+// Because Dog is an Animal (inheritance), C++ allows assigning a Dog* to an Animal*.
+
+// So the pointer p itself stores the address of the Dog object, but its type is Animal*.
+  // p (Animal*) --> points to Dog object
+  
+  // cout<<*p;//error as p doesnt have fixed byte like int so compiler doesnt know to print 
 }
 // Not allowed as p is pointing towards animal class and it doesnt have roti function 
+
+
+// Bacially Animal type p pointer point to dog type object 
+// during compile time it decide to run animal method but virtual->runtime it 
+// have a vtable and vpointer made during constructor of dog (every object of that class shares it)
+// and vpointer seraches for the original method from dog and runs it   Dog::speak() 

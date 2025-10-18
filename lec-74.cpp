927d61c467d563f219ba90136757eb8f0be56e5c
Pratham Mevada <prathammevada34@gmail.com>
2025-10-18 12:18:57 +0530

    commit 9 inheritance

--- lec-74.cpp
+++ lec-74.cpp
@@ -6,51 +6,75 @@ class Human
   public:
   string name;
 
-  Human(string name)
+  void display()
   {
-    this->name=name;
+    cout<<"My name is "<<name<<"\n";
   }
-};
-
-class A
-{
-  int propA;
-
   
 };
 
-class Student:public Human,public A
+
+class Engineer:public virtual Human
 {
-  int roll;
+  
 
   public:
-  Student(string name,int roll):Human(name) 
+  int roll;
+
+  void work()
   {
-    this->roll=roll;
-    cout<<name<<" "<<roll<<"\n";
+    cout<<"My roll is "<<roll<<"\n";
+
   }
+ 
+};
+
+class Youtuber:public virtual Human
+{
+  public:
+  int subscribers;
+
+  void content()
+  {
+        cout<<"My subs is "<<subscribers<<"\n";
+
+
+  }
+  
 
 };
 
 
-class Teacher:public Human
+class Teacher:public Youtuber,public Engineer
 {
   int salary;
 
   public:
-  Teacher(string name,int salary):Human(name)
-  {
+  Teacher(int subscribers,int roll,string name,int salary){
+
+    this->name=name;//error as from both youtuber and engineer name is coming from both
+    //to solve it write virtual in both Youtuber and engineer classes
+    // same goes for display method but virtual solves it for more go ahead he teaches it ahead
+    this->subscribers=subscribers;
+    this->roll=roll;
     this->salary=salary;
-    cout<<name<<" "<<salary<<"\n";
+
+
+
   }
 
 };
 
 
+
+
+
+
 int main()
 {
+
   
 }
-// Hybrid INheritance
-// Multiple+hybrid 
+// Multipath INheritance
+// Human inherited by Youtuber and engineer and teacher inherit both youtuber and engineer 
 

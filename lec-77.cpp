230c404d73db29f78fa7ea519c9f27fe7336ca9e
Pratham Mevada <prathammevada34@gmail.com>
2025-10-18 19:10:14 +0530

    Commit 1

--- lec-77.cpp
+++ lec-77.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 
 
 int main()
 {
-
+  ofstream fout;
+  fout.open("zoom.txt");//if present then open else creates it
+  // Created in the directory working
+  fout<<"Hello India";
+  fout.close();//Resource release 
 }
 
 // File handling

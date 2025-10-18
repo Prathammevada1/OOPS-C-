3d5b604a911831ba7d580862f7be837c57c7f14f
Pratham Mevada <prathammevada34@gmail.com>
2025-10-18 18:38:18 +0530

    commit-6

--- lec-76.cpp
+++ lec-76.cpp
@@ -1,104 +1,42 @@
 #include <iostream>
-
 using namespace std;
 
-<<<<<<< HEAD
-class Exception
+class InvalidAmount_Error:public runtime_error
 {
-  protected:
-  string msg;
-
   public:
-
-  Exception(string msg)
-  {
-    this->msg = msg;
-  }
-
-  string what()
-  {
-    return msg;
-  }
-};
-
-class Runtime_error:public Exception
-{
-  Runtime_error(const string &msg):Exception(msg)
+  InvalidAmount_Error(const string &msg):runtime_error(msg)
   {
 
   }
 };
-
-//this might what it would look like
-// throw the object of runtime() constructor is called and it calls exception
-// constructor and msg value is allocated and it is catch at const runtime_error &e
-// here in e the object comes and e.what() prints msg in e
-
+//&msg is used cause if only msg is used two copies are made one when padding msg to runtime exception
+// and other while making string for INvalid amount constructor
+ 
 class Customer
 {
   int balance;
 
   public:
-  Customer(int balance)
+  Customer(int bal)
   {
-    this->balance=balance;
+    balance = bal;
   }
 
-  void withdraw(int amount)
+  void withdraw(int amt)
   {
-    if(amount<0)
-    throw runtime_error("Cant withdraw");
-    
-  }
-=======
-class exception
-{
-  protected:
-  string msg;
-
-  public:
-
-  exception(string msg)
-  {
-    this->msg = msg;
-  }
-
-  string what()
-  {
-    return msg;
+    if(amt<0)
+    throw InvalidAmount_Error("Not valid amount");
   }
 };
 
-
-
-
 int main()
 {
-<<<<<<< HEAD
-
-  Customer c1(1000);
-  try{
+  Customer c1(2000);
+  try
+  {  
     c1.withdraw(-100);
-  }catch(const runtime_error &e)//here the object is received throw runtime_error("")
+  }catch(const InvalidAmount_Error &e)
   {
     cout<<e.what();
   }
-  catch(...)
-  {
-
-  }
-  // default catch
-=======
-  try
-  {
-    int *p=new int[100000000000000];
-    delete []p;
-  }catch(const bad_alloc &e)
-  {
-    cout<<"Exception occur due to line 9 "<<e.what();
-
-  }
->>>>>>> c9de8c47886cfa38acc885ad0c40a82dfc6f3839
-
 }
-//what exception class looks like roughly

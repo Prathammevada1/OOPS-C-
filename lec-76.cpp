962e537a751b6b9b943f0ef6e110925fcc5749f4
Pratham Mevada <prathammevada34@gmail.com>
2025-10-18 17:30:25 +0530

    commit-2

--- lec-76.cpp
+++ lec-76.cpp
@@ -1,54 +1,60 @@
 #include <iostream>
 using namespace std;
 
-class Customer
-{
-  string name;
-  int balance,acc_num;
-  public:
+// class Customer
+// {
+//   string name;
+//   int balance,acc_num;
+//   public:
 
-  Customer(string name,int balance,int acc_num)
-  {
-    this->name=name;
-    this->balance=balance;
-    this->acc_num=acc_num;
-  }
+//   Customer(string name,int balance,int acc_num)
+//   {
+//     this->name=name;
+//     this->balance=balance;
+//     this->acc_num=acc_num;
+//   }
 
-  void deposit(int amount)
-  {
-      if(amount>0)
-      {
-        balance+=amount;
-        cout<<"Amount credited\n";
-      }
-      else
-      {
-        throw "Invalid deposited amount";//if throw runs it terminate and doesnt run rest of the method
-      }
-  }
+//   void deposit(int amount)
+//   {
+//       if(amount>0)
+//       {
+//         balance+=amount;
+//         cout<<"Amount credited\n";
+//       }
+//       else
+//       {
+//         throw "Invalid deposited amount";//if throw runs it terminate and doesnt run rest of the method
+//       }
+//   }
 
-  void withdraw(int amount)
-  {
-      if(amount>0&&amount<balance)
-      {
-        balance-=amount;
-        cout<<"Debited credited\n";
-      }
-  }
+//   // void withdraw(int amount)
+//   // {
+//   //     if(amount>0&&amount<balance)
+//   //     {
+//   //       balance-=amount;
+//   //       cout<<"Debited credited\n";
+//   //     }
+//   // }
 
 
-};
+// };
 
 int main()
 {
-  Customer c1("Pratham",5000,10);
-  try
-  {
-    c1.deposit(100);
-    c1.deposit(-100);
-    c1.withdraw(-100);//never reached as before it the exception is handled
+
+
+  int a,b;
+  cin>>a>>b;
+  try{
+    if(b==0)
+    throw "Cant divide by zero";
+    int c=a/b;
+    cout<<c;
+    
   }catch(const char *e)
   {
-    cout<<"Exception "<<e;
+    cout<<e;
   }
+
+  return 0; 
 }
